b2f6b73f71f8eff3617792e84772fb915e4f26e4
xuanle0298 <31940681+xuanle0298@users.noreply.github.com>
2017-09-20 17:57:09 -0700

    payroll9

--- test/payrolltest\payroll_test.cpp
+++ test/payrolltest\payroll_test.cpp
@@ -1,1 +1,9 @@
+#include "gtest/gtest.h"
+#include "Payroll.h"
 
+TEST(hours_returned, hours_larger_than_40)
+  EXPECTED_EQ(20, getOvertimeHours(60));
+
+TEST(overtime_pay_returned, overtime_hours)
+  EXPECTED_EQ(300, getOvertimePay(10));
+ 
