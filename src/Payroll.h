bdc1a1e9ed7c9231a43f757b8795e76aa67cba78
xuanle0298 <31940681+xuanle0298@users.noreply.github.com>
2017-09-20 17:00:03 -0700

    Payroll Implementation2

--- src/Payroll.h
+++ src/Payroll.h
@@ -1,7 +1,19 @@
 #ifndef _PAYROLL_H
 #define _PAYROLL_H
 
-
-
+int getRegularHours();
+int getOvertimeHours(int);
+float getRegularPay(double regHours, double regRate);
+float getOvertimePay(double otHours, double otRate);
+float getGrossPay(double regPay, double otPay);
+double getFIT(double fit, double grossPay);
+double getFICASSN(double ficassn, double grossPay);
+double getFICAMED(double ficamed, double grossPay);
+double getNetPay(double grossPay, double fit, double ficassn, double ficamed);
+double getSalary();
+double getFIT_S(double grossPayS, double FIT);
+double getFICASSN_S(double SS, double grossPayS);
+double getFICAMED_S(double med, double grossPayS);
+double getNetPay_S(double grossPayS, double fitTaxS, double ssnTaxS, double medTaxS);
 
 #endif
