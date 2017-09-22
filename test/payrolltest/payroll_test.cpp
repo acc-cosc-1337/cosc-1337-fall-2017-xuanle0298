ba38664a785e90b6abe1572ea6e023ee9b6f6e18
xuanle0298 <31940681+xuanle0298@users.noreply.github.com>
2017-09-22 16:06:43 -0700

    Update payroll_test.cpp

--- test/payrolltest/payroll_test.cpp
+++ test/payrolltest/payroll_test.cpp
@@ -2,10 +2,10 @@
 #include "Payroll.h"
 
 TEST(hours_returned, hours_larger_than_40){
-    EXPECT_EQ(10, getOvertimeHours(50));
+    EXPECT_EQ(10, getOvertimeHours(50, 40));
 }
 
-TEST(overtime_pay_returned, overtime_hours){
-    EXPECT_EQ(300, getOvertimePay(10, OT_RATE));
+TEST(overtime_pay_returned, overtime_hours, overtimeRate){
+    EXPECT_EQ(300, getOvertimePay(10, 30));
 }
  
