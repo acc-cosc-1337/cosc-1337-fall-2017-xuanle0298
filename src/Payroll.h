60c6b1690614f0fcb9f35f955dbe83faf5b7eab8
xuanle0298 <31940681+xuanle0298@users.noreply.github.com>
2017-09-22 17:29:31 -0700

    Update Payroll.h

--- src/Payroll.h
+++ src/Payroll.h
@@ -1,18 +1,18 @@
 #ifndef _PAYROLL_H
 #define _PAYROLL_H
 
-int getRegularHours();
-int getOvertimeHours(int Hours, int ST_HOURS);
-double getRegularPay(int ST_HOURS, double stRate);
-double getOvertimePay(int otOvertimeHrs, double OT_RATE);
-double getGrossPay(double regPay, double otPay);
-double getFIT(double FIT, double grossPay);
-double getFICASSN(double SS, double grossPay);
-double getFICAMED(double MED, double grossPay);
-double getNetPay(double grossPay, double fitTax, double ssnTax, double medTax);
-double getFIT_S(double grossPayS, double FIT);
-double getFICASSN_S(double SS, double grossPayS);
-double getFICAMED_S(double MED, double grossPayS);
-double getNetPay_S(double grossPayS, double fitTaxS, double ssnTaxS, double medTaxS);
+static int getRegularHours();
+static int getOvertimeHours(int Hours, int ST_HOURS);
+static double getRegularPay(int ST_HOURS, double stRate);
+static double getOvertimePay(int otOvertimeHrs, double OT_RATE);
+static double getGrossPay(double regPay, double otPay);
+static double getFIT(double FIT, double grossPay);
+static double getFICASSN(double SS, double grossPay);
+static double getFICAMED(double MED, double grossPay);
+static double getNetPay(double grossPay, double fitTax, double ssnTax, double medTax);
+static double getFIT_S(double grossPayS, double FIT);
+static double getFICASSN_S(double SS, double grossPayS);
+static double getFICAMED_S(double MED, double grossPayS);
+static double getNetPay_S(double grossPayS, double fitTaxS, double ssnTaxS, double medTaxS);
 
 #endif
